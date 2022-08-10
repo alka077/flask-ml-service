d39a34537ed0da3152a102f5c0b4c76cd99ed997
alka077 <106584802+alka077@users.noreply.github.com>
2022-08-10 14:57:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home: from azure pipelines</h3>"
+    html = "<h3>Sklearn Prediction Home: from Azure pipelines</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
